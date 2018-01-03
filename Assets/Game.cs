0d5154f24a455abf35fb4e8f86e273a0cc25c1e7
swatichanchal <swatisingh.chanchal@gamil.com>
2018-01-03 16:00:01 +0530

    my game

---
+++ Assets/Game.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Game : MonoBehaviour
+{
+    public GameObject cube;
+    public Vector3 jumpforce;
+    // Use this for initialization
+    void Start()
+    {
+        jumpforce = new Vector3(0f, 50f, 0f);
+
+    }
+    void OnTriggerEnter()
+    {
+        print("Collision Detected");
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Jump"))
+        {
+            cube.GetComponent<Rigidbody>().AddForce(jumpforce);
+        }
+    }
+
+}
